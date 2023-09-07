cbb3117d9f08fad8181a3f9aa95aafe679395cac
laghibi <khalidlaghribi99@gmail.com>
2023-09-07 18:01:17 +0100

    finission gmail to admin

--- contact.php
+++ contact.php
@@ -236,7 +236,7 @@ if (isset($_POST['add'])) {
       <form  method="post" class="space-y-8">
       <div>
               <label for="Name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your name</label>
-              <input type="name" id="name" name="Nom" value="<?php echo $Nom;?>" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Your name" required>
+              <input type="name" id="name" name="Nom" value="<?php echo $Nom;?>" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Full name" required>
           </div>         
           <div>
               <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
